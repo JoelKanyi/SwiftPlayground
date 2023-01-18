baa39c1bab542bd52f8d2763fe749da6727c419a
JoelKanyi <joelkanyi98@gmail.com>
2023-01-18 08:46:06 +0300

    variables, types, constants, operators

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+// Variables
+var playerName = "Ava"
+var isEnabled = false
+var age: Int = 24
 
-var greeting = "Hello, playground"
+// Variables without a value
+var carSource: Int
+var name: String
+var isOld: Bool
+var temp: Double
+
+
+// Constant
+let year = 2023
+
+// Must be intialized before using it
+let temparature: Double
+
+
+// Operators
+// +, -, *, /
+
+
+// Function
+func addTwoNumbers(a: Int, b: Int) -> Int {
+    return a + b
+}
+
+
+print(addTwoNumbers(a: 20, b: 30))
