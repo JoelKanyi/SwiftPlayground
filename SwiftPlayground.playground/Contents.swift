206993b44b6515933d1d5f3619c426ea4bdf8d23
JoelKanyi <joelkanyi98@gmail.com>
2023-01-20 10:18:27 +0300

    enums, structs, dictionaries and tuples

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -163,3 +163,126 @@ _ = add(num1: 20, num2: 40)
 
 // Get type function
 type(of: add(num1: 24, num2: 32))
+
+
+/*
+ Enumerations, Structs, Dictionries, Tuples
+ */
+
+// Enums
+enum MediaType {
+    case book, movie, music, game
+}
+
+var itemName: String // e.g "Starwars"
+var releaseYear: Int // e.g 2004
+var itemType: MediaType // e.g book, movie, game
+
+itemType = MediaType.movie
+itemType = .game
+
+switch itemType {
+case .game:
+    print("I have been playing games")
+case .music:
+    print("I have been listening to music")
+case .movie:
+    print("I have been watching movies")
+case .book:
+    print("I have been reading books")
+}
+
+// Enums: Raw Values
+enum BottleSize: String {
+    case half = "37.5l"
+    case full = "50.5l"
+}
+
+
+var myBottle: BottleSize = .half
+print("\(myBottle.rawValue)")
+
+// Enum: Associated Values
+enum Music {
+    case gospel(String)
+    case drills(Int)
+    case bongo(String)
+}
+
+var itemOne: Music = .bongo("Diamond Platnumz")
+var itemTwo: Music = .gospel("Sammy K")
+
+
+
+// Structs --> Value type not refrence type
+struct Movie {
+    // properties
+    var title: String
+    var director: String
+    var releaseYear: Int
+    var genre: String
+    
+    // methods
+    func summary() -> String {
+        return "\(title) directed by \(director) is a \(genre) release in \(releaseYear)"
+    }
+}
+
+var firstMovie = Movie(title: "The Rain", director: "Joel K.", releaseYear: 2019, genre: "Sci-fi")
+
+print(firstMovie.title)
+
+print(firstMovie.summary())
+
+
+
+// Dictionaries
+var airlines = [
+    "KQ": "Kenya Airways",
+    "RA": "Rwanda Airline"
+]
+
+// lookup
+if let result = airlines["KQ"] {
+    print(result)
+} else {
+    print("No value found")
+}
+
+// add or change
+airlines["RA"] = "Rwanda Express"
+airlines["QA"] = "Qatar Airline"
+
+// remove
+airlines["KQ"] = nil
+
+// iterate through
+for (_, value) in airlines {
+    print(value)
+}
+
+
+// Tuples - Grooup things together
+var numberOfCows = 10
+var overEighteenYears = true
+var todaysTemp = "23.4f"
+
+var basicTuple = (numberOfCows, overEighteenYears, todaysTemp)
+
+var nextTuple = ("Joel", true, 23434, todaysTemp)
+
+
+func randomAlbum() -> (title: String,  year :Int) {
+    var title = "Premium tears"
+    var releaseYear = 2018
+    
+    return (title, releaseYear)
+}
+
+var tupleResult = randomAlbum()
+print(tupleResult.title)
+
+let (nextTitle, length) = randomAlbum()
+print("Playing next: \(nextTitle) which was release in \(length)")
+
+
